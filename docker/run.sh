52c70975b15fd29ac22d29875739642b01d96d97
Manuel Vacelet <manuel.vacelet@enalean.com>
2015-10-23 16:52:39 +0200

    Dockerize

---
+++ docker/run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -ex
+
+wait_for_db() {
+    while ! mysql -h$DB_PORT_3306_TCP_ADDR -uroot -p$MYSQL_ROOT_PASSWORD -e "show databases" >/dev/null; do 
+	echo "Wait for the db";
+	sleep 1
+    done
+}
+
+db_exists() {
+    db=$(mysql -h$DB_PORT_3306_TCP_ADDR -uroot -p$MYSQL_ROOT_PASSWORD -e "show databases like 'cfp'")
+    if [ -z "$db" ]; then
+	false
+    else
+	true
+    fi
+}
+
+create_db() {
+    if ! db_exists; then
+	mysql -h$DB_PORT_3306_TCP_ADDR -uroot -p$MYSQL_ROOT_PASSWORD -e "create database cfp"
+    fi
+}
+
+update_configuration_files() {
+    sed -i "s%host: 127.0.0.1%host: $DB_PORT_3306_TCP_ADDR%" /app/config/production.yml
+    sed -i "s%dsn:.*%dsn: mysql:dbname=cfp;host=$DB_PORT_3306_TCP_ADDR%" /app/config/production.yml
+
+
+    sed -i "s%host: localhost%host: $DB_PORT_3306_TCP_ADDR%" phinx.yml
+    sed -i "s%pass: ''%pass: $MYSQL_ROOT_PASSWORD%" phinx.yml
+}
+
+run_migration() {
+    cd /app
+    vendor/bin/phinx migrate --environment=production
+}
+
+wait_for_db
+create_db
+update_configuration_files
+run_migration
